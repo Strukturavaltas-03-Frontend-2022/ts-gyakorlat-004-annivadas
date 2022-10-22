b283900ba85b69251acb55d460a8d8645cb02d6f
annivadas <anni.vadas@gmail.com>
2022-10-22 10:21:10 +0200

    done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-// Importáld be a HumanHero és TransformerHero osztályokat.
-
+// Importáld be a HumanHero és TransformerHero osztályokat. 
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,8 +9,31 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
-
+//export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    {
+      id: 1,
+      name: "John",
+      sex: "male",
+      age: 11,
+      health: 1,
+    },
+    {
+      id: 2,
+      name: "Jim",
+      sex: "male",
+      age: 22,
+      health: 2,
+    },
+    {
+      id: 3,
+      name: "Jack",
+      sex: "male",
+      age: 33,
+      health: 3,
+    },
+  ];
+  
 /**
  * Állítsd be helyesen a változót!
  * Neve: transformers
@@ -17,4 +41,28 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+//export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    {
+      id: 3,
+      name: "Odysseus",
+      wings: 0,
+      wheels: 2,
+      clan: "greek",
+    },
+    {
+      id: 3,
+      name: "Agamennon",
+      wings: 0,
+      wheels: 0,
+      clan: "greek",
+    },
+    {
+      id: 3,
+      name: "Paris",
+      wings: 6,
+      wheels: 0,
+      clan: "Trojan",
+    },
+  ];
+  
